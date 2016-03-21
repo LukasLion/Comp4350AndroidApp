cee854b086957bbbaf63634488fd49f7e278db61
A F M Zunaid <zunaid2998@gmail.com>
2016-03-21 15:06:23 -0500

    MainActivity is added. So service call for profiles and conferences are added in this commit and also necessary handlers and switching activities. Will show some error. We need to merge this with the fragments.

--- app/src/main/java/com/umanitoba/comp4350androidapp/MainActivity.java
+++ app/src/main/java/com/umanitoba/comp4350androidapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.umanitoba.comp4350androidapp;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Message;
@@ -25,10 +26,83 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.os.Message;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
+
+    //Ahmed code started ------------------------------------------------//
+    public ArrayList<Conference> conferenceList;
+    public ArrayList<Profile> profileList;
+
+    public void showAdapter(ArrayList<String> array) {
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, array);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    public void showProfileAdapter(ArrayList<String> array) {
+        ListView listView = (ListView) findViewById(R.id.listView2);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, array);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    public void getConferenceList(final ConferenceListener conferenceListener){
+        final String url = "http://ec2-52-37-252-126.us-west-2.compute.amazonaws.com/api/ConferencesService";
+        // Request a string response
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        conferenceListener.onSuccessListener(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        // Add the request to the queue
+        Request<String> queue = Volley.newRequestQueue(this).add(stringRequest);
+    }
+
+    public void getProfileList(final ProfilesListener profilesListener){
+        final String url = "http://ec2-52-37-252-126.us-west-2.compute.amazonaws.com/api/ProfilesService";
+        // Request a string response
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        profilesListener.onSuccessListener(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        // Add the request to the queue
+        Request<String> queue = Volley.newRequestQueue(this).add(stringRequest);
+    }
+
+    // Ahmed code ended.-----------------------------//
+
 
     private FragmentTabHost mTabHost;
     private String userToken = "empty";
@@ -41,6 +115,117 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // coded by Ahmed. this part is port generating the conference and profile list and redirecting the user to the details acitivity.
+
+        final ListView profileView = (ListView) findViewById((R.id.listView2));
+        final ListView conferenceView = (ListView) findViewById((R.id.listView));
+
+        Button button = (Button) findViewById(R.id.getAllconferences);
+        Button buttonProfiles = (Button) findViewById(R.id.getAllProfiles);
+
+        buttonProfiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                conferenceView.setVisibility(View.GONE);
+                profileView.setVisibility(View.VISIBLE);
+
+                ProfilesListener profilesListener = new ProfilesListener() {
+                    @Override
+                    public void onSuccessListener(String result) {
+                        ArrayList<Profile> profiles = new ArrayList<Profile>();
+                        ArrayList<String> myListView = new ArrayList<String>();
+
+                        try {
+
+                            JSONArray array = new JSONArray(result);
+
+                            for (int i = 0; i < array.length(); i++) {
+                                JSONObject object = array.getJSONObject(i);
+                                Profile profile = new Profile(Integer.parseInt(object.getString("ProfileId")), object.getString("UserId"), Integer.parseInt(object.getString("Age")), object.getString("City"), object.getString("Country"), object.getString("Degree"), object.getString("FirstName"), object.getString("LastName"),object.getString("School"));
+                                profiles.add(profile);
+                                myListView.add(profile.getFirstName() + " " + profile.getLastName());
+                            }
+
+                            showProfileAdapter(myListView);
+                            profileList = profiles;
+
+                        } catch(JSONException e){
+                            System.out.println(e.toString());
+                        }
+                    }
+                };
+
+                getProfileList(profilesListener);
+            }
+        });
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                profileView.setVisibility(View.GONE);
+                conferenceView.setVisibility(View.VISIBLE);
+
+                ConferenceListener conferenceListener = new ConferenceListener() {
+                    @Override
+                    public void onSuccessListener(String result) {
+
+                        ArrayList<Conference> conferences = new ArrayList<Conference>();
+                        ArrayList<String> myListView = new ArrayList<String>();
+
+                        try {
+
+                            JSONArray array = new JSONArray(result);
+
+                            for (int i = 0; i < array.length(); i++) {
+                                JSONObject object = array.getJSONObject(i);
+                                Conference conference = new Conference(Integer.parseInt(object.getString("ConferenceId")), object.getString("Content"), object.getString("Date"), object.getString("FirstName"), object.getString("LastName"), object.getString("Location"), Integer.parseInt(object.getString("ProfileId")),object.getString("Title"));
+                                conferences.add(conference);
+                                myListView.add(object.getString("Date").substring(0, 10) + "  " + object.getString("Title"));
+                            }
+
+                            showAdapter(myListView);
+                            conferenceList = conferences;
+
+                        } catch(JSONException e){
+                            System.out.println(e.toString());
+                        }
+                    }
+                };
+                getConferenceList(conferenceListener);
+            }
+        });
+
+        ListView lv = (ListView) findViewById(R.id.listView);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, ConferenceActivity.class);
+                Conference conference = conferenceList.get(position);
+                intent.putExtra("Conference", conference);
+                startActivity(intent);
+            }
+        });
+
+        ListView lv2 = (ListView) findViewById(R.id.listView2);
+        lv2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                Bundle bundle = new Bundle();
+                Profile profile = profileList.get(position);
+                bundle.putSerializable("Profile", profile);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+        //code ended by Ahmed-------------------------------------------------------------//
 
         SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
         hasUser = preferences.getBoolean("UserSignedIn", false);
